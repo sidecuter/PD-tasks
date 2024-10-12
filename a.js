6305794b61613ae75f238e5d64aa477d276798c5
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2024-10-12 22:04:47 +0300

    task А

--- a.js
+++ a.js
@@ -1,1 +1,9 @@
-console.log("а")
+import { readFileSync } from "fs"
+
+let data = readFileSync("./semester.json")
+
+//Вообще судя по подсказкам IDE у буфера есть метод toJSON, но мы в сферическом вакуме строк
+let parsed = JSON.parse(data.toString())
+//let parsed = data.toJSON()
+
+export const json = JSON.stringify(parsed.contents)
