e08928430f588958e118effe3902c9c92053416f
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2024-10-13 17:21:00 +0300

    Change return type of function

--- task2/main.js
+++ task2/main.js
@@ -1,7 +1,20 @@
 import { parseGroups } from "./helpers/parseGroups.js";
 
+/**
+ * @function parse - Парсит содержимое расписания и предоставляет занятия по аудиториям
+ * @param {Object} contents - содержимое поля contents расписания
+ * @param {Array{String}} auditories - список аудиторий
+ * @returns Object
+ */
 export const parse = (contents, auditories) => {
     let state = new Map;
+    auditories = new Set(auditories);
     parseGroups(contents, state, auditories);
-    return [...state].map(([_, aud]) => aud);
+    return Object.fromEntries(state.entries());
 }
+// Пример вызова функции
+// import { json } from "../task1/a.js";
+// import { auditories } from "../task1/b.js";
+// const content = JSON.parse(json)
+// const auds = auditories(content)
+// parse(content, auds)
