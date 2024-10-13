2e21be52e3d10666217301d5e009c279de311e6b
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2024-10-13 16:30:48 +0300

    Remove unused imports and development variables

--- task2/main.js
+++ task2/main.js
@@ -1,15 +1,7 @@
-import {json} from "../task1/a.js"
-import {auditories} from "../task1/b.js"
 import { parseGroups } from "./helpers/parseGroups.js";
-
-const contents = JSON.parse(json);
-const auds = new Set(auditories(contents))
 
 export const parse = (contents, auditories) => {
     let state = new Map;
     parseGroups(contents, state, auditories);
     return [...state].map(([_, aud]) => aud);
 }
-
-const state = parse(contents, auds);
-console.log(JSON.stringify(state));
