1c9874fefa17454d457fe8a99799eb3e42442117
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2024-10-13 17:19:41 +0300

    useless module

--- task2/models/models.js
+++
@@ -1,6 +0,0 @@
-import {Auditory} from "./aud.js";
-import {Grid} from "./grid.js";
-import {Day} from "./day.js";
-import {Lesson} from "./lesson.js";
-
-module.exports = Auditory, Grid, Day, Lesson
