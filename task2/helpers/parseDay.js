2e95b66fd4f97e1232a87547b2daace7fee85ca6
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2024-10-13 17:20:40 +0300

    Code refactor

--- task2/helpers/parseDay.js
+++ task2/helpers/parseDay.js
@@ -1,45 +1,28 @@
 import { parseLesson } from "./parseLesson.js";
-import { findAuditory } from "./findAuditory.js";
+import { Auditory } from "../models/aud.js";
+
+const dayNumToName = {
+    "1": "monday",
+    "2": "tuesday",
+    "3": "wednesday",
+    "4": "thursday",
+    "5": "friday",
+    "6": "saturday",
+};
 
 export const parseDay = (dayNum, day, group, state, auditories) => {
     Object.entries(day).forEach(([lessonNum, lesson]) => {
-        if (lesson.length) {
-            try {
-                let [lessonCont, auds] = parseLesson(lesson, group);
-                auds.forEach(aud => {
-                    aud = aud.toLowerCase();
-                    let auditory = findAuditory(aud, auditories, state);
-                    try {
-                        switch (dayNum) {
-                            case "1":
-                                auditory.rasp.monday[lessonNum] = lessonCont;
-                                auditory.rasp.monday.lessons.push(lessonNum);
-                                break;
-                            case "2":
-                                auditory.rasp.tuesday[lessonNum] = lessonCont;
-                                auditory.rasp.tuesday.lessons.push(lessonNum);
-                                break;
-                            case "3":
-                                auditory.rasp.wednesday[lessonNum] = lessonCont;
-                                auditory.rasp.wednesday.lessons.push(lessonNum);
-                                break;
-                            case "4":
-                                auditory.rasp.thursday[lessonNum] = lessonCont;
-                                auditory.rasp.thursday.lessons.push(lessonNum);
-                                break;
-                            case "5":
-                                auditory.rasp.friday[lessonNum] = lessonCont;
-                                auditory.rasp.friday.lessons.push(lessonNum);
-                                break;
-                            case "6":
-                                auditory.rasp.saturday[lessonNum] = lessonCont;
-                                auditory.rasp.saturday.lessons.push(lessonNum);
-                                break;
-                        }
-                        state.set(aud, auditory);
-                    } catch (_) {}
-                })
-            } catch (_) {}
-        }
+        let [lessonCont, auds] = parseLesson(lesson, group);
+        auds.forEach(aud => {
+            aud = aud.toLowerCase();
+            if (auditories.has(aud)) {
+                if (!state.has(aud)) {
+                    state.set(aud, new Auditory);
+                    state.get(aud).id = aud;
+                }
+                state.get(aud).rasp[dayNumToName[dayNum]][lessonNum] = lessonCont;
+                state.get(aud).rasp[dayNumToName[dayNum]].lessons.push(lessonNum);
+            }
+        });
     });
 }
