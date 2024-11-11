a8267000fd93fe9734b3e137edeb77f4d27c12e9
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2024-11-11 11:44:17 +0300

    Rewrite data fetching

--- auditories/a.js
+++ auditories/a.js
@@ -1,9 +1,16 @@
 import { readFileSync } from "fs"
 
-let data = readFileSync("./semester.json");
-
-//Вообще судя по подсказкам IDE у буфера есть метод toJSON, но мы в сферическом вакуме строк
-let parsed = JSON.parse(data.toString());
-//let parsed = data.toJSON()
+let parsed = await fetch("https://rasp.dmami.ru/semester.json",
+    {
+        headers: {
+            Authority: "rasp.dmami.ru",
+            Referer: "https://rasp.dmami.ru"
+        }
+    })
+    .then(result => result.json())
+    .catch(error => {
+        console.error(`File fetching failed with error: ${error}. Using fallback source`);
+        return JSON.parse(readFileSync("./semester.json").toString());
+    });
 
 export const json = JSON.stringify(parsed.contents);
