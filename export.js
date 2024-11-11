5b70f369a097e85c3f82c620d82449d6828e1294
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2024-11-11 11:12:19 +0300

    Rename module task2 to more self-explanatory name

--- export.js
+++ export.js
@@ -1,4 +1,4 @@
-import { parse } from "./task2/main.js"
+import { parse } from "./auditories/main.js"
 import {json} from "./task1/a.js";
 import {auditories} from "./task1/b.js";
 import {writeFileSync} from "fs";
