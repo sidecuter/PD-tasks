d3583081054aa3f61dbac074e257fbdf2da80c74
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2024-11-11 19:58:00 +0300

    Add test unit for file generation

---
+++ name_unifier/test.js
@@ -0,0 +1,53 @@
+import {is_valid, unify} from "./main.js";
+import { json } from "../auditories/data_fetching.js";
+import {writeFileSync} from "fs";
+
+const filter_reg = /(пд|зал|cпорт|онлайн|лайн|федеральная|имаш|hami|нами|техноград|биокомбинат|сколково|биотехнологии|hами)/i;
+
+const auditories = (contents, count = null) => {
+    let keys = Object.keys(contents);
+    if (count !== null && typeof count === 'number') {
+        keys = keys.slice(0, count);
+    }
+    let auds = {};
+    keys.forEach(group => {
+        auds = {...parseGroup(contents[group]), ...auds}
+        //auds.add(...parseGroup(contents[group]));
+    });
+    // let a = {};
+    // Array(...auds).map((aud, kanonize) => a[aud] = kanonize);
+    return auds;
+}
+
+const parseGroup = (group) => {
+    let auds = {};
+    Object.values(group.grid).forEach(day =>
+        auds = {...parseDay(day), ...auds}
+    );
+    return auds;
+}
+
+const parseDay = (day) => {
+    let auds = {};
+    Object.values(day).forEach(para => {
+        //Парсим аудитории с пары
+        para
+            .filter(variant => is_valid(variant.location))
+            .forEach(variant => {
+                variant
+                    .shortRooms
+                    .filter(room => !room.toLowerCase().match(filter_reg))
+                    .forEach(
+                        room => {
+                            let [key, value] = unify(variant.location, room.toLowerCase());
+                            auds[key] = value
+                        }
+                );
+        });
+    });
+    return auds;
+}
+
+const data = JSON.parse(json);
+
+writeFileSync("auditories.json", JSON.stringify(auditories(data)));
