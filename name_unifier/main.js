42572d0cd0b1e435e42945744494762317b3689f
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2024-11-11 19:57:23 +0300

    Finish work for unify module

--- name_unifier/main.js
+++ name_unifier/main.js
@@ -1,4 +1,4 @@
-import helpers from "./helpers.js";
+import {av, bs, m, pr, pk} from "./helpers.js";
 
 const LOCATION_NAMES = [
     "автозаводская",
@@ -9,15 +9,17 @@ const LOCATION_NAMES = [
 ];
 
 const PATTERNS = {
-    "автозаводская": helpers.av,
-    "большая семеновская": helpers.bs,
-    "михалковская": helpers.m,
-    "прянишникова": helpers.pr,
-    "павла корчагина": helpers.pk
+    "автозаводская": av,
+    "большая семеновская": bs,
+    "михалковская": m,
+    "прянишникова": pr,
+    "павла корчагина": pk
 };
 
-const is_valid = (location) => LOCATION_NAMES.some(loc => loc == location.trim().toLowerCase());
+export const is_valid = (location) => LOCATION_NAMES.some(loc => loc == location.trim().toLowerCase());
 
-const a = (location, auditorie) => {
-
+export const unify = (location, auditorie) => {
+    return [auditorie, PATTERNS[location.trim().toLowerCase()](auditorie)]
 };
+
+export default {is_valid, unify};
