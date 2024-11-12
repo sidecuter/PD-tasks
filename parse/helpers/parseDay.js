5bbf755c9bef6a4adbe40a4608a994347763d950
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2024-11-12 16:37:04 +0300

    Reduce iterations by excluding lessons without variants and valid locations

--- parse/helpers/parseDay.js
+++ parse/helpers/parseDay.js
@@ -1,6 +1,6 @@
 import { parseLesson } from "./parseLesson.js";
 import { Auditory } from "../models/aud.js";
-import { unify } from "../../name_unifier/main.js"
+import { is_valid } from "../../name_unifier/main.js"
 
 // Нужно для более лаконичного доступа к полям моделей
 const dayNumToName = {
@@ -21,26 +21,27 @@ const dayNumToName = {
  * @param {Object} auditories 
  */
 export const parseDay = (dayNum, day, group, state, auditories) => {
-    // Перебираем все пары за день (они записаны в формате 'номер': {Объекты вариантов})
-    Object.entries(day).forEach(([lessonNum, lesson]) => {
-        // Получаем спаршенную модель пары и аудитории для нее
-        let [lessonCont, auds, location] = parseLesson(lesson, group);
-        if (location = "" || auds.length == 0) return;
-        // Перебираем все аудитории
-        auds
-            .map(v => v.toLowerCase()) // Преобразуем названия аудиторий к нижнему регистру
-            .filter(v => v in auditories) // Отфильтровываем те, которых нет в массиве аудиторий
-            .map(v => auditories[v])
-            .forEach(aud => {
-                // Если в состоянии еще нет такой аудитории, то создаем новый объект и пишем его в состояние
-                if (!state.has(aud)) {
-                    state.set(aud, new Auditory);
-                    state.get(aud).id = aud;
-                }
-                // Записываем для дня dayNum информацию о паре lessonNum
-                state.get(aud).rasp[dayNumToName[dayNum]][lessonNum] = lessonCont;
-                // Дополняем массив lessons информацией о номере пары
-                state.get(aud).rasp[dayNumToName[dayNum]].lessons.push(lessonNum);
-        });
+    // Перебираем все пары за день (они записаны в формате 'номер': {Объект вариантов})
+    Object.entries(day)
+        .filter(([_, lesson]) => Object.values(lesson).some(v => is_valid(v.location)))
+        .forEach(([lessonNum, lesson]) => {
+            // Получаем спаршенную модель пары и аудитории для нее
+            let [lessonCont, auds, location] = parseLesson(lesson, group);
+            if (location = "" || auds.length == 0) return;
+            // Перебираем все аудитории
+            auds
+                .filter(v => v.toLowerCase() in auditories) // Отфильтровываем те, которых нет в объекте аудиторий
+                .forEach(aud => {
+                    aud = auditories[aud.toLowerCase()];
+                    // Если в состоянии еще нет такой аудитории, то создаем новый объект и пишем его в состояние
+                    if (!state.has(aud)) {
+                        state.set(aud, new Auditory);
+                        state.get(aud).id = aud;
+                    }
+                    // Записываем для дня dayNum информацию о паре lessonNum
+                    state.get(aud).rasp[dayNumToName[dayNum]][lessonNum] = lessonCont;
+                    // Дополняем массив lessons информацией о номере пары
+                    state.get(aud).rasp[dayNumToName[dayNum]].lessons.push(lessonNum);
+            });
     });
 }
