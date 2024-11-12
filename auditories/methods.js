d97e0493922cb581f9c5795edbde1fa298c2b628
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2024-11-12 23:07:35 +0300

    Remove unnecessary returns by state usage

--- auditories/methods.js
+++ auditories/methods.js
@@ -1,41 +1,34 @@
 import {is_valid, unify} from "../name_unifier/main.js";
+import use_state from "../state/main.js";
 
 const filter_reg = /(пд|зал|cпорт|онлайн|лайн|федеральная|имаш|hami|нами|техноград|биокомбинат|сколково|биотехнологии|hами)/i;
+
+const [getAuds, _, updateAuds] = use_state({});
 
 export const auditories = (contents, count = null) => {
     let keys = Object.keys(contents);
     if (count !== null && typeof count === 'number') {
         keys = keys.slice(0, count);
     }
-    let auds = {};
-    keys.forEach(group => {
-        auds = {...parseGroup(contents[group]), ...auds}
-    });
-    return auds;
+    keys.forEach(group => parseGroup(contents[group]));
+    return {...getAuds()};
 }
 
 
-const parseGroup = (group) => {
-    let auds = {};
-    Object.values(group.grid).forEach(day =>
-        auds = {...parseDay(day), ...auds}
-    );
-    return auds;
-}
+const parseGroup = (group) => Object.values(group.grid).forEach(day => parseDay(day));
 
 const parseDay = (day) => {
-    let auds = {};
     Object.values(day).forEach(para => para
         //Парсим аудитории с пары
         .filter(variant => is_valid(variant.location))
         .forEach(variant => variant
             .shortRooms
             .filter(room => !room.toLowerCase().match(filter_reg))
-            .forEach(room => {
+            .filter(room => !(room in getAuds()))
+            .forEach(room => updateAuds(state => {
                 let [key, value] = unify(variant.location, room.toLowerCase());
-                auds[key] = value
-            })
+                state[key] = value;
+            }))
         )
     );
-    return auds;
 }
--- auditories/test.js
+++ auditories/test.js
@@ -4,3 +4,5 @@ import {auditories} from "./methods.js";
 const contents = JSON.parse(json);
 
 const spaces = auditories(contents);
+
+console.log(spaces);
