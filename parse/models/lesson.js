c1b5e8480edb2e7065d000609cc7cb1717ff34a8
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2024-11-13 00:10:38 +0300

    Remove value duplication

--- parse/models/lesson.js
+++ parse/models/lesson.js
@@ -1,13 +1,23 @@
 /**
  * Хранит информацию о занятии
- * @field {string} groupName - название учебной группы/направления пд
+ * @field {Array{string}} groupNames - названия учебных групп/направлений пд
  * @field {string} groupType - тип группы (Study/pd)
  * @field {string} discipline - название дисциплины
  * @field {Array{string}} teachers - преподаватели
 */
 export class Lesson {
-    groupName = "";
+    groupNames = [];
     groupType = "";
     discipline = "";
     teachers = [];
+
+    /**
+     * @field {Lesson} lesson - урок для слияния
+     */
+    merge(lesson) {
+        if (lesson.groupType == this.groupType &&
+            lesson.discipline == this.discipline &&
+            this.groupNames.lastIndexOf(lesson.groupNames[0]) == -1
+        ) this.groupNames.push(lesson.groupNames[0]);
+    }
 }
